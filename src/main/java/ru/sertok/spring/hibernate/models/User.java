422cea67a0a279fa6b919567521aff30e4c8c65f
sertokinson <sertokinson@gmail.com>
2018-11-14 17:07:17 +0300

    добавил модель

---
+++ src/main/java/ru/sertok/spring/hibernate/models/User.java
@@ -0,0 +1,32 @@
+package ru.sertok.spring.hibernate.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+
+@Entity
+@Table(name = "fix_user")
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "birthdate")
+    private Date birthDate;
+}
